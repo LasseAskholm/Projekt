45fdc7d46b0466bf1942c07ba3b24ea36fa45026
Akselthyregod <59265356+Akselthyregod@users.noreply.github.com>
2020-11-21 12:19:53 +0100

    Implemented Interface

--- src/com/company/BuildingSystem.java
+++ src/com/company/BuildingSystem.java
@@ -1,10 +1,8 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
-public class BuildingSystem {
+public class BuildingSystem implements IBuildingManagementSystem{
 
     private List<Building> buildings;
 
@@ -29,5 +27,81 @@ public class BuildingSystem {
 
     public void removeBuilding(UUID id){
         buildings.removeIf(building -> building.getID() == id);
+    }
+
+    @Override
+    public Map<UUID, String> getBuildingInformation() {
+        Map<UUID, String> map = new HashMap<>();
+        for ( Building building :
+                buildings ) {
+            map.put(building.getID(), building.getName());
+        }
+        return map;
+    }
+
+    @Override
+    public Map<UUID, String> getSensorInformation(UUID buildingId) {
+        Map<UUID, String> sensors = new HashMap<>();
+
+        //List<Sensor> sensorsList = building.getSensors();
+        for ( Sensor sensor :
+                findBuilding(buildingId).getSensors() ) {
+
+            sensors.put(sensor.getID(), sensor.getName());
+        }
+
+        return sensors;
+    }
+
+    @Override
+    public Map<UUID, String> getActuatorInformation(UUID buildingId) {
+        Map<UUID, String> actuators = new HashMap<>();
+
+        //List<Sensor> sensorsList = building.getSensors();
+        for ( Actuator actuator :
+                findBuilding(buildingId).getActuators() ) {
+
+            actuators.put(actuator.getID(), actuator.getName());
+        }
+
+        return actuators;
+    }
+
+    @Override
+    public UUID addTemperatureSensor(UUID buildingId, String name) {
+
+        return findBuilding(buildingId).addTemperatureSensor(name);
+    }
+
+    @Override
+    public UUID addCo2Sensor(UUID buildingId, String name) {
+
+    return findBuilding(buildingId).addCo2Sensor(name);
+
+    }
+
+    @Override
+    public void removeSensor(UUID buildingId, UUID sensorId) {
+        findBuilding(buildingId).removeSensor(sensorId);
+    }
+
+    @Override
+    public UUID addVentilationActuator(UUID buildingId, String name) {
+        return findBuilding(buildingId).addVentActuator(name);
+    }
+
+    @Override
+    public void removeActuator(UUID buildingId, UUID actuatorId) {
+        findBuilding(buildingId).removeActuator(actuatorId);
+    }
+
+    public Building findBuilding(UUID buildingId){
+        for ( Building building :
+                buildings ) {
+            if (building.getID() == buildingId) {
+                return building;
+            }
+        }
+        return null;
     }
 }
